060682c2bdbf0921185749f7c05fab1cda5ce794
sokolovgit <sanok.sokolov928100@gmail.com>
2024-11-04 18:24:29 +0200

    feat(lab2): turned in laboratory work

--- lab2/code/roles.sql
+++ lab2/code/roles.sql
@@ -1,15 +1,15 @@
-CREATE ROLE db_admin;
-CREATE ROLE manager;
-CREATE ROLE driver;
+    CREATE ROLE db_admin;
+    CREATE ROLE manager;
+    CREATE ROLE driver;
 
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO db_admin;
-GRANT SELECT, INSERT, UPDATE, DELETE ON team, driver, vehicle TO manager;
-GRANT SELECT ON passenger_record, route TO driver;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO db_admin;
+    GRANT SELECT, INSERT, UPDATE, DELETE ON team, driver, vehicle TO manager;
+    GRANT SELECT ON passenger_record, route TO driver;
 
-CREATE USER admin_user WITH PASSWORD 'admin_password';
-CREATE USER manager_user WITH PASSWORD 'manager_password';
-CREATE USER driver_user WITH PASSWORD 'driver_password';
+    CREATE USER admin_user WITH PASSWORD 'admin_password';
+    CREATE USER manager_user WITH PASSWORD 'manager_password';
+    CREATE USER driver_user WITH PASSWORD 'driver_password';
 
-GRANT db_admin TO admin_user;
-GRANT manager TO manager_user;
-GRANT driver TO driver_user;
+    GRANT db_admin TO admin_user;
+    GRANT manager TO manager_user;
+    GRANT driver TO driver_user;
