4d7e3b15ff1392cd876795549293814a01f7b877
sokolovgit <sanok.sokolov928100@gmail.com>
2024-12-16 23:49:39 +0200

    feat(course_work): add vehicle types data

---
+++ course_work/code/vehicle_types.sql
@@ -0,0 +1,14 @@
+INSERT INTO vehicle_types (name, description) VALUES
+('Car', 'Passenger car used for personal or commercial purposes.'),
+('Motorcycle', 'Two-wheeled motor vehicle.'),
+('Truck', 'Motor vehicle designed to transport goods or materials.'),
+('Bus', 'Motor vehicle designed to carry multiple passengers.'),
+('Bicycle', 'Two-wheeled vehicle powered by pedaling.'),
+('Electric Scooter', 'Two-wheeled electric-powered vehicle for short-distance travel.'),
+('Public Transport', 'Vehicles used for public transportation, such as trams or trolleybuses.'),
+('Special Purpose Vehicle', 'Vehicles such as fire trucks, ambulances, or police cars.'),
+('Trailer', 'Unpowered vehicle towed by another vehicle.'),
+('Agricultural Vehicle', 'Vehicles such as tractors used for farming purposes.'),
+('Electric Car', 'Vehicle powered exclusively by electricity.'),
+('ATV', 'All-terrain vehicle used for off-road travel.'),
+('Dump Truck', 'Truck designed to transport and unload materials.');
