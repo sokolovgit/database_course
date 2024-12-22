4b37890234fe77fae48677a7324ac1ef0d472ed6
sokolovgit <sanok.sokolov928100@gmail.com>
2024-12-22 04:15:03 +0200

    feat(course_work): update vehilce types data

--- course_work/code/vehicle_types.sql
+++ course_work/code/vehicle_types.sql
@@ -1,12 +1,12 @@
-INSERT INTO vehicle_types (name, description) VALUES
-('Car', 'Passenger car used for personal or commercial purposes.'),
-('Motorcycle', 'Two-wheeled motor vehicle.'),
-('Truck', 'Motor vehicle designed to transport goods or materials.'),
-('Bus', 'Motor vehicle designed to carry multiple passengers.'),
-('Electric Scooter', 'Two-wheeled electric-powered vehicle for short-distance travel.'),
-('Special Purpose Vehicle', 'Vehicles such as fire trucks, ambulances, or police cars.'),
-('Trailer', 'Unpowered vehicle towed by another vehicle.'),
-('Agricultural Vehicle', 'Vehicles such as tractors used for farming purposes.'),
-('Electric Car', 'Vehicle powered exclusively by electricity.'),
-('ATV', 'All-terrain vehicle used for off-road travel.'),
-('Dump Truck', 'Truck designed to transport and unload materials.');
+INSERT INTO vehicle_types (name, description, min_seating_capacity, max_seating_capacity, min_engine_capacity, max_engine_capacity) VALUES
+('Car', 'Passenger car used for personal or commercial purposes.', 2, 8, 1.0, 6.0),
+('Motorcycle', 'Two-wheeled motor vehicle.', 1, 2, 0.1, 2.0),
+('Truck', 'Motor vehicle designed to transport goods or materials.', 2, 2, 2.0, 15.0),
+('Bus', 'Motor vehicle designed to carry multiple passengers.', 9, NULL, 4.0, 15.0),
+('Electric Scooter', 'Two-wheeled electric-powered vehicle for short-distance travel.', 1, 1, NULL, NULL),
+('Special Purpose Vehicle', 'Vehicles such as fire trucks, ambulances, or police cars.', 1, 10, 2.0, 10.0),
+('Trailer', 'Unpowered vehicle towed by another vehicle.', NULL, NULL, NULL, NULL),
+('Agricultural Vehicle', 'Vehicles such as tractors used for farming purposes.', 1, 3, 1.0, 5.0),
+('Electric Car', 'Vehicle powered exclusively by electricity.', 2, 8, NULL, NULL),
+('ATV', 'All-terrain vehicle used for off-road travel.', 1, 2, 0.5, 1.5),
+('Dump Truck', 'Truck designed to transport and unload materials.', 2, 2, 2.0, 15.0);
