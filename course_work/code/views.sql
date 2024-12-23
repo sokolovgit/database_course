eed1c97c76b3d43f3da1283077a673a220d24ee7
sokolovgit <sanok.sokolov928100@gmail.com>
2024-12-23 14:47:37 +0200

    refactor(course_work): format views

--- course_work/code/views.sql
+++ course_work/code/views.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE VIEW violation_details AS
+CREATE
+OR REPLACE VIEW violation_details AS
 SELECT
     v.id AS violation_id,
     get_citizen_full_name(c.id) AS driver_name,
@@ -11,18 +12,16 @@ SELECT
     get_administrative_offense_info(administrative_offense_id) AS administrative_offense,
     ao.description AS administrative_offense_description,
     ao.penalty_fee
+FROM
+    violations v
+    JOIN vehicles veh ON v.vehicle_id = veh.id
+    JOIN citizens c ON veh.owner_id = c.id
+    JOIN locations loc ON v.location_id = loc.id
+    JOIN traffic_rules tr ON v.traffic_rule_id = tr.id
+    JOIN administrative_offenses ao ON v.administrative_offense_id = ao.id;
 
-
-FROM violations v
-JOIN vehicles veh ON v.vehicle_id = veh.id
-JOIN citizens c ON veh.owner_id = c.id
-JOIN locations loc ON v.location_id = loc.id
-JOIN traffic_rules tr ON v.traffic_rule_id = tr.id
-JOIN administrative_offenses ao ON v.administrative_offense_id = ao.id;
-
-
-
-CREATE OR REPLACE VIEW full_vehicle_info AS
+CREATE
+OR REPLACE VIEW full_vehicle_info AS
 SELECT
     v.id AS vehicle_id,
     v.registration_number,
@@ -32,39 +31,52 @@ SELECT
     v.year_of_manufacture,
     v.vin,
     v.insurance_policy_number,
-    v.engine_type,
     v.engine_capacity,
     v.seating_capacity,
     get_citizen_full_name(v.owner_id) AS owner_full_name,
     CASE
-        WHEN get_plate_type(v.registration_number) = 'regular' THEN get_region_by_code(SUBSTRING(v.registration_number FROM 1 FOR 2))
+        WHEN get_plate_type(v.registration_number) = 'regular' THEN get_region_by_code(
+            SUBSTRING(
+                v.registration_number
+                FROM
+                    1 FOR 2
+            )
+        )
         ELSE 'N/A'
     END AS region
 FROM
     vehicles v;
 
-
-
-CREATE OR REPLACE VIEW driver_violation_summary AS
+CREATE
+OR REPLACE VIEW driver_violation_summary AS
 SELECT
     c.id AS driver_id,
     get_citizen_full_name(c.id) AS driver_name,
-    COUNT(CASE WHEN ap.id IS NULL AND ar.id IS NULL THEN 1 END) AS violations_without_document,
-    COUNT(CASE WHEN ap.id IS NOT NULL AND ar.id IS NULL THEN 1 END) AS violations_with_protocol,
-    COUNT(CASE WHEN ar.id IS NOT NULL THEN 1 END) AS violations_with_resolution,
+    COUNT(
+        CASE
+            WHEN ap.id IS NULL
+            AND ar.id IS NULL THEN 1
+        END
+    ) AS violations_without_document,
+    COUNT(
+        CASE
+            WHEN ap.id IS NOT NULL
+            AND ar.id IS NULL THEN 1
+        END
+    ) AS violations_with_protocol,
+    COUNT(
+        CASE
+            WHEN ar.id IS NOT NULL THEN 1
+        END
+    ) AS violations_with_resolution,
     COUNT(v.id) AS total_violations,
     SUM(ao.penalty_fee) AS total_penalty_fees
 FROM
     citizens c
-JOIN
-    vehicles veh ON c.id = veh.owner_id
-JOIN
-    violations v ON veh.id = v.vehicle_id
-JOIN
-    administrative_offenses ao ON v.administrative_offense_id = ao.id
-LEFT JOIN
-    accident_protocols ap ON v.id = ap.violation_id
-LEFT JOIN
-    accident_resolutions ar ON v.id = ar.violation_id
+    JOIN vehicles veh ON c.id = veh.owner_id
+    JOIN violations v ON veh.id = v.vehicle_id
+    JOIN administrative_offenses ao ON v.administrative_offense_id = ao.id
+    LEFT JOIN accident_protocols ap ON v.id = ap.violation_id
+    LEFT JOIN accident_resolutions ar ON v.id = ar.violation_id
 GROUP BY
     c.id;
