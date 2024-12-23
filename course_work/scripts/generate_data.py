9e700a2d560945a768fded696e3d68b6020877c5
sokolovgit <sanok.sokolov928100@gmail.com>
2024-12-23 14:44:25 +0200

    fix(course_work): update script for generating data

--- course_work/scripts/generate_data.py
+++ course_work/scripts/generate_data.py
@@ -1,4 +1,6 @@
+from calendar import c
 import csv
+from itertools import count
 import random
 from secrets import choice
 import string
@@ -11,6 +13,36 @@ from faker_vehicle import VehicleProvider
 
 fake = Faker("uk_UA")
 fake.add_provider(VehicleProvider)
+
+regions = [
+    {"name": "AR Krym", "codes": ["AK", "KK", "TK"]},
+    {"name": "Vinnytska oblast", "codes": ["AB", "KB", "IM"]},
+    {"name": "Volynska oblast", "codes": ["AC", "KC", "CM"]},
+    {"name": "Dnipropetrovska oblast", "codes": ["AE", "KE", "PP"]},
+    {"name": "Donetska oblast", "codes": ["AH", "KH", "TH"]},
+    {"name": "Zhytomyrska oblast", "codes": ["AM", "KM", "TM"]},
+    {"name": "Zakarpatska oblast", "codes": ["AO", "KO", "MT"]},
+    {"name": "Zaporizka oblast", "codes": ["AP", "KP", "TP"]},
+    {"name": "Ivano-Frankivska oblast", "codes": ["AT", "KT", "TO"]},
+    {"name": "Kyivska oblast", "codes": ["AI", "KI", "TI"]},
+    {"name": "misto Kyiv", "codes": ["AA", "KA", "TT"]},
+    {"name": "Kirovohradska oblast", "codes": ["BA", "HA", "XA"]},
+    {"name": "Luhanska oblast", "codes": ["BB", "HB", "EP"]},
+    {"name": "Lvivska oblast", "codes": ["BC", "HC", "CC"]},
+    {"name": "Mykolaivska oblast", "codes": ["BE", "HE", "XE"]},
+    {"name": "Odeska oblast", "codes": ["BH", "HH", "OO"]},
+    {"name": "Poltavska oblast", "codes": ["BI", "HI", "XI"]},
+    {"name": "Rivnenska oblast", "codes": ["BK", "HK", "XK"]},
+    {"name": "Sumska oblast", "codes": ["BM", "HM", "XM"]},
+    {"name": "Ternopilska oblast", "codes": ["BO", "HO", "XO"]},
+    {"name": "Kharkivska oblast", "codes": ["AX", "KX", "XX"]},
+    {"name": "Khersonska oblast", "codes": ["BT", "HT", "XT"]},
+    {"name": "Khmelnytska oblast", "codes": ["BX", "HX", "OX"]},
+    {"name": "Cherkaska oblast", "codes": ["CA", "IA", "OA"]},
+    {"name": "Chernihivska oblast", "codes": ["CB", "IB", "OB"]},
+    {"name": "Chernivetska oblast", "codes": ["CE", "IE", "OE"]},
+    {"name": "misto Sevastopol", "codes": ["CH", "IH", "OH"]},
+]
 
 
 def fetch_violation_times():
@@ -29,7 +61,7 @@ def fetch_violation_times():
 
     try:
         conn = psycopg2.connect(
-            "postgresql://postgres:password@localhost:5432/course_work"
+            "postgresql://postgres:tec928100@localhost:5432/course_work"
         )
         cursor = conn.cursor()
 
@@ -57,25 +89,27 @@ def fetch_violation_times():
 # Генерація даних для таблиці citizens
 def generate_citizens(filename, count):
     citizens = []
-    for _ in range(count):
+    for i in range(count):
         date_of_birth = fake.date_of_birth(minimum_age=1, maximum_age=100)
         gender = random.choice(["male", "female"])
 
         citizen = {
-            "id": None,  # Primary key auto-incremented
+            "id": i + 1,  # Primary key auto-incremented
             "first_name": (
                 fake.first_name_male() if gender == "male" else fake.first_name_female()
             ),
             "last_name": (
-                fake.last_name() if gender == "male" else fake.last_name_female()
+                fake.last_name_male() if gender == "male" else fake.last_name_female()
             ),
             "patronymic": (
-                fake.middle_name() if gender == "male" else fake.middle_name_female()
+                fake.middle_name_male()
+                if gender == "male"
+                else fake.middle_name_female()
             ),
             "date_of_birth": date_of_birth.strftime("%Y-%m-%d"),
         }
 
-        print(f"adding {citizen}")
+        print(f"{i} adding {citizen}")
 
         citizens.append(citizen)
 
@@ -89,7 +123,7 @@ def generate_citizens(filename, count):
 def generate_drivers(filename, citizens_count, count):
     drivers = []
     used_citizen_ids = set()
-    for _ in range(count):
+    for i in range(count):
         citizen_id = random.randint(1, citizens_count)
         while citizen_id in used_citizen_ids:
             citizen_id = random.randint(1, citizens_count)
@@ -101,7 +135,7 @@ def generate_drivers(filename, citizens_count, count):
         )
 
         driver = {
-            "id": None,  # Primary key auto-incremented
+            "id": i + 1,  # Primary key auto-incremented
             "citizen_id": citizen_id,
             "license_number": license_number,
             "license_issued_time": license_issued_time.strftime("%Y-%m-%d %H:%M:%S"),
@@ -120,6 +154,7 @@ def generate_drivers(filename, citizens_count, count):
 def generate_police_officers(filename, citizens_count, count):
     police_officers = []
     citizen_ids = random.sample(range(1, citizens_count + 1), count)
+    counter = 0
 
     # Define ranks and their weights
     ranks = [
@@ -141,9 +176,9 @@ def generate_police_officers(filename, citizens_count, count):
         rank = random.choices(ranks, weights=weights, k=1)[
             0
         ]  # Choose a rank with weights
-
+        counter += 1
         police_officer = {
-            "id": None,  # Primary key auto-incremented
+            "id": counter,  # Primary key auto-incremented
             "citizen_id": citizen_id,
             "badge_number": badge_number,
             "rank": rank,  # Use just the rank without weights
@@ -160,71 +195,135 @@ def generate_police_officers(filename, citizens_count, count):
 
 
 # Генерація даних для таблиці vehicles
-def generate_vehicles(filename, citizens_count, vehicle_types_count, count):
+def generate_vehicles(filename, citizens_count, count):
     vehicles = []
-    for _ in range(count):
+    vehicle_type_data = {
+        1: {
+            "min_seating": 2,
+            "max_seating": 8,
+            "min_engine": 1.00,
+            "max_engine": 6.00,
+        },  # Car
+        2: {
+            "min_seating": 2,
+            "max_seating": 8,
+            "min_engine": None,
+            "max_engine": None,
+        },  # Electric Car
+        3: {
+            "min_seating": 2,
+            "max_seating": 8,
+            "min_engine": 1.00,
+            "max_engine": 6.00,
+        },  # Hybrid Vehicle
+        4: {
+            "min_seating": 1,
+            "max_seating": 2,
+            "min_engine": 0.10,
+            "max_engine": 2.00,
+        },  # Motorcycle
+        5: {
+            "min_seating": 2,
+            "max_seating": 15,
+            "min_engine": 2.00,
+            "max_engine": 6.00,
+        },  # Van
+        6: {
+            "min_seating": 2,
+            "max_seating": 2,
+            "min_engine": 2.00,
+            "max_engine": 15.00,
+        },  # Truck
+        7: {
+            "min_seating": 9,
+            "max_seating": None,
+            "min_engine": 4.00,
+            "max_engine": 15.00,
+        },  # Bus
+        8: {
+            "min_seating": 1,
+            "max_seating": 10,
+            "min_engine": 2.00,
+            "max_engine": 10.00,
+        },  # Special Purpose Vehicle
+        9: {
+            "min_seating": None,
+            "max_seating": None,
+            "min_engine": None,
+            "max_engine": None,
+        },  # Trailer
+        10: {
+            "min_seating": 1,
+            "max_seating": 3,
+            "min_engine": 1.00,
+            "max_engine": 5.00,
+        },  # Agricultural Vehicle
+        11: {
+            "min_seating": 1,
+            "max_seating": 2,
+            "min_engine": 0.50,
+            "max_engine": 1.50,
+        },  # ATV
+        12: {
+            "min_seating": 2,
+            "max_seating": 2,
+            "min_engine": 2.00,
+            "max_engine": 15.00,
+        },  # Dump Truck
+    }
+
+    for i in range(count):
         owner_id = random.randint(1, citizens_count)
-        vehicle_type_id = random.randint(1, vehicle_types_count)
+        vehicle_type_id = random.randint(
+            1, len(vehicle_type_data)
+        )  # Get vehicle type ID from the data
 
-        choice = fake.random_element(
-            [
-                ("standart", 0.9),
-                ("custom", 0.1),
-            ]
-        )
+        choice = random.choices(["standard", "custom"], weights=[9, 1])[0]
 
-        if choice == "standart":
-            registration_number = fake.bothify("??####??", letters="ABCEHIKMOPTX")
+        if choice == "standard":
+            region = random.choice(regions)
+            region_code = random.choice(region["codes"])
+            registration_number = fake.bothify(
+                f"{region_code}####??", letters="ABCEHIKMOPTX"
+            )
         else:
             length = random.randint(3, 8)
             registration_number = "".join(
                 random.choices(string.ascii_uppercase + string.digits, k=length)
             )
+            registration_number = "".join(
+                random.sample(registration_number, len(registration_number))
+            )
 
         vin = fake.vin()
-        engine_type = random.choice(["gasoline", "diesel", "electric", "hybrid"])
+        type_data = vehicle_type_data.get(vehicle_type_id, {})
         engine_capacity = (
-            None if engine_type == "electric" else round(random.uniform(1.0, 5.0), 2)
+            round(random.uniform(type_data["min_engine"], type_data["max_engine"]), 2)
+            if type_data.get("min_engine") and type_data.get("max_engine")
+            else None
         )
-        # Determine seating capacity based on vehicle type
-        if vehicle_type_id in [1, 9]:  # Car or Electric Car
-            seating_capacity = random.randint(2, 8)
-        elif vehicle_type_id == 2:  # Motorcycle
-            seating_capacity = random.randint(1, 2)
-        elif vehicle_type_id == 4:  # Bus
-            seating_capacity = random.randint(9, 50)  # Assuming max 50 for buses
-        elif vehicle_type_id == 3:  # Truck
-            seating_capacity = 2
-        elif vehicle_type_id == 7:  # Trailer
-            seating_capacity = None
-        elif vehicle_type_id == 6:  # Special Purpose Vehicle
-            seating_capacity = random.randint(1, 10)
-        elif vehicle_type_id == 11:  # Dump Truck
-            seating_capacity = 2
-        elif vehicle_type_id == 8:  # Agricultural Vehicle
-            seating_capacity = random.randint(1, 3)
-        elif vehicle_type_id == 10:  # ATV
-            seating_capacity = random.randint(1, 2)
-        elif vehicle_type_id == 5:  # Electric Scooter
-            seating_capacity = 1
-        else:
-            seating_capacity = random.randint(1, 8)  # Default case
+        seating_capacity = (
+            random.randint(type_data["min_seating"], type_data["max_seating"])
+            if type_data.get("min_seating") and type_data.get("max_seating")
+            else None
+        )
 
         vehicle_object = fake.vehicle_object()
 
         vehicle = {
-            "id": None,  # Primary key auto-incremented
+            "id": i + 1,
             "owner_id": owner_id,
             "vehicle_type_id": vehicle_type_id,
             "registration_number": registration_number,
             "vin": vin,
-            "insurance_policy_number": fake.bothify(
-                "???######", letters="ABCEHIKMOPTX"
+            "insurance_policy_number": (
+                fake.bothify("???######", letters="ABCEHIKMOPTX")
+                if random.random() < 0.9
+                else None
             ),
             "model": vehicle_object["Model"],
             "brand": vehicle_object["Make"],
             "color": fake.color_name(),
-            "engine_type": engine_type,
             "engine_capacity": engine_capacity,
             "seating_capacity": seating_capacity,
             "year_of_manufacture": vehicle_object["Year"],
@@ -250,9 +349,9 @@ def generate_locations(filename, count):
     min_lat, max_lat = 44.0, 52.5
     min_lon, max_lon = 22.0, 40.5
 
-    for _ in range(count):
+    for i in range(count):
         location = {
-            "id": None,  # Primary key auto-incremented
+            "id": i + 1,  # Primary key auto-incremented
             "latitude": round(random.uniform(min_lat, max_lat), 6),
             "longitude": round(random.uniform(min_lon, max_lon), 6),
             "street": fake.street_name(),  # Generates a Ukrainian street name
@@ -278,7 +377,7 @@ def generate_violations(filename, vehicles_count, locations_count, count):
 
     violations = []
 
-    for _ in range(count):
+    for i in range(count):
         vehicle_id = random.randint(1, vehicles_count)
         location_id = random.randint(1, locations_count)
         administrative_offense_id = random.randint(1, administrative_offenses_count)
@@ -286,7 +385,7 @@ def generate_violations(filename, vehicles_count, locations_count, count):
         time_of_violation = fake.date_time_between(start_date="-10y", end_date="now")
 
         violation = {
-            "id": None,  # Primary key auto-incremented
+            "id": i + 1,  # Primary key auto-incremented
             "time_of_violation": time_of_violation.strftime("%Y-%m-%d %H:%M:%S"),
             "description": (
                 fake.text(max_nb_chars=200) if random.random() < 0.7 else None
@@ -310,14 +409,17 @@ def generate_violations(filename, vehicles_count, locations_count, count):
 def generate_evidences(filename, violations_count):
     evidences = []
 
+    counter = 0
     for violation_id in range(1, violations_count + 1):
         num_evidences = random.choices(
             range(11), weights=[1, 2, 3, 4, 5, 4, 3, 2, 1, 1, 1], k=1
         )[0]
 
         for _ in range(num_evidences):
+            counter += 1
+
             evidence = {
-                "id": None,  # Primary key auto-incremented
+                "id": counter,  # Primary key auto-incremented
                 "violation_id": violation_id,
                 "type": random.choice(["photo", "video"]),
                 "url": fake.image_url(),
@@ -341,9 +443,10 @@ def generate_accident_protocols(
     count,
 ):
     protocols = []
+    counter = 0
 
     # Fetch violation times from the database
-    violation_times = fetch_violation_times()
+    violation_times, protocol_violation_ids = fetch_violation_times()
 
     for _ in range(count):
         # Randomly pick a violation ID
@@ -363,8 +466,18 @@ def generate_accident_protocols(
             start_date=time_of_violation, end_date="now"
         ).strftime("%Y-%m-%d %H:%M:%S")
 
+        # Ensure police officer is not the same as the defendant
+        police_officer_id = random.randint(1, police_officers_count)
+        defendant_id = random.randint(1, citizens_count)
+
+        # If the selected police officer is the same as the defendant, change the police officer ID
+        while police_officer_id == defendant_id:
+            police_officer_id = random.randint(1, police_officers_count)
+
+        counter += 1
+
         protocol = {
-            "id": None,  # Primary key auto-incremented
+            "id": counter,  # Primary key auto-incremented
             "series": fake.lexify(
                 text="??", letters="ABCEHIKMOPTX"
             ),  # Random 2-letter series
@@ -374,8 +487,8 @@ def generate_accident_protocols(
             ),
             "time_of_drawing_up": time_of_drawing_up,
             "violation_id": violation_id,
-            "police_officer_id": random.randint(1, police_officers_count),
-            "defendant_id": random.randint(1, citizens_count),
+            "police_officer_id": police_officer_id,
+            "defendant_id": defendant_id,
         }
 
         print(f"adding {protocol}")
@@ -393,6 +506,7 @@ def generate_citizens_on_protocol(filename, citizens_count, protocols_count):
     roles = ["witness", "victim"]  # Example roles
 
     citizens_protocols = []
+    counter = 0
 
     for _ in range(protocols_count):
 
@@ -401,9 +515,10 @@ def generate_citizens_on_protocol(filename, citizens_count, protocols_count):
         )[0]
 
         for _ in range(num_of_citizens_on_protocol):
+            counter += 1
 
             citizen_on_protocol = {
-                "id": None,  # Primary key auto-incremented
+                "id": counter,  # Primary key auto-incremented
                 "role": random.choice(roles),
                 "citizen_id": random.randint(1, citizens_count),
                 "protocol_id": random.randint(1, protocols_count),
@@ -426,7 +541,7 @@ def generate_accident_resolutions(
     filename, violations_count, police_officers_count, locations_count, count
 ):
     resolutions = []
-
+    counter = 0
     # Fetch violation data
     violation_times, protocol_violation_ids = fetch_violation_times()
 
@@ -456,8 +571,10 @@ def generate_accident_resolutions(
             start_date=time_of_consideration, end_date="+30d"
         )
 
+        counter += 1
+
         resolution = {
-            "id": None,  # Primary key auto-incremented
+            "id": counter,  # Primary key auto-incremented
             "series": fake.lexify("??", letters="ABCEHIKMOPTX"),
             "number": fake.numerify("######"),
             "time_of_consideration": time_of_consideration.strftime(
@@ -484,21 +601,21 @@ def generate_accident_resolutions(
 
 # Виклик функцій для генерації даних
 if __name__ == "__main__":
-    citizens_count = 10000
-    drivers_count = 9000
-    police_officer_count = 1000
+    citizens_count = 20000
+    drivers_count = 10000
+    police_officer_count = 2000
     vehicles_count = 15000
     location_count = 5000
-    violations_count = 20000
-    protocols_count = 9000
-    resolutions_count = 9000
+    violations_count = 30000
+    protocols_count = 14000
+    resolutions_count = 14000
 
-    # generate_citizens("../csv/citizens.csv", citizens_count)
     # generate_drivers("../csv/drivers.csv", citizens_count, drivers_count)
+    # generate_citizens("../csv/citizens.csv", citizens_count)
     # generate_police_officers(
     #     "../csv/police_officers.csv", citizens_count, police_officer_count
     # )
-    # generate_vehicles("../csv/vehicles.csv", citizens_count, 11, vehicles_count)
+    # generate_vehicles("../csv/vehicles.csv", citizens_count, vehicles_count)
     # generate_locations("../csv/locations.csv", location_count)
 
     # generate_violations(
@@ -506,19 +623,19 @@ if __name__ == "__main__":
     # )
 
     # generate_evidences("../csv/evidences.csv", violations_count)
-    # generate_accident_protocols(
-    #     "../csv/accident_protocols.csv",
-    #     violations_count,
-    #     police_officer_count,
-    #     citizens_count,
-    #     protocols_count,
-    # )
+    generate_accident_protocols(
+        "../csv/accident_protocols.csv",
+        violations_count,
+        police_officer_count,
+        citizens_count,
+        protocols_count,
+    )
 
-    # generate_citizens_on_protocol(
-    #     "../csv/citizens_on_protocol.csv",
-    #     citizens_count,
-    #     protocols_count=protocols_count,
-    # )
+    generate_citizens_on_protocol(
+        "../csv/citizens_on_protocol.csv",
+        citizens_count,
+        protocols_count=protocols_count,
+    )
 
     generate_accident_resolutions(
         "../csv/accident_resolutions.csv",
