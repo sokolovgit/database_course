8e53cec92de6adef2a05f68dd647efd9e266ff5c
sokolovgit <sanok.sokolov928100@gmail.com>
2024-12-23 23:59:20 +0200

    feat(lab5): add triggers

---
+++ lab5/code/triggers.sql
@@ -0,0 +1,72 @@
+-- a. Тригер на видалення даних
+
+CREATE OR REPLACE FUNCTION log_vehicle_deletion()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO vehicle_operations_log (operation, vehicle_id, model, operation_date)
+    VALUES ('DELETE', OLD.id, OLD.model, CURRENT_TIMESTAMP);
+
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER vehicle_delete_trigger
+AFTER DELETE ON vehicle
+FOR EACH ROW
+EXECUTE FUNCTION log_vehicle_deletion();
+
+
+INSERT INTO vehicle (id, registration_number, model, brand, year_of_manufacture, status, vehicle_type_id, capacity, load_capacity, team_id)
+VALUES (33333, 'AB123CD', 'Test Model', 'Test Brand', 2020, 'available', 1, 50, 1000, 1);
+
+DELETE FROM vehicle WHERE id = 33333;
+
+SELECT * FROM vehicle_operations_log WHERE operation = 'DELETE';
+
+-- b. Тригер на модифікацію даних
+
+CREATE OR REPLACE FUNCTION log_vehicle_update()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO vehicle_operations_log (operation, vehicle_id, old_status, new_status, operation_date)
+    VALUES ('UPDATE', OLD.id, OLD.status, NEW.status, CURRENT_TIMESTAMP);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER vehicle_update_trigger
+AFTER UPDATE ON vehicle
+FOR EACH ROW
+EXECUTE FUNCTION log_vehicle_update();
+
+
+INSERT INTO vehicle (id, registration_number, model, brand, year_of_manufacture, status, vehicle_type_id, capacity, load_capacity, team_id)
+VALUES (33334, 'AB123CD', 'Test Model', 'Test Brand', 2020, 'available', 1, 50, 1000, 1);
+
+UPDATE vehicle SET status = 'available' WHERE id = 33334;
+
+SELECT * FROM vehicle_operations_log WHERE operation = 'UPDATE';
+
+
+--  c. Тригер на додавання даних
+
+CREATE OR REPLACE FUNCTION log_vehicle_insert()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO vehicle_operations_log (operation, vehicle_id, model, operation_date)
+    VALUES ('INSERT', NEW.id, NEW.model, CURRENT_TIMESTAMP);
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER vehicle_insert_trigger
+AFTER INSERT ON vehicle
+FOR EACH ROW
+EXECUTE FUNCTION log_vehicle_insert();
+
+INSERT INTO vehicle (id, registration_number, model, brand, year_of_manufacture, status, vehicle_type_id, capacity, load_capacity, team_id)
+VALUES (33335, 'AB1645CD', 'Test Model', 'Test Brand', 2020, 'available', 1, 50, 1000, 1);
+
+SELECT * FROM vehicle_operations_log WHERE operation = 'INSERT';
